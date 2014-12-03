28436d6939da666aa98849350ea122e21db3168b
Chris Ho <chris.kee@hotmail.com>
2014-12-03 16:30:51 +0800

    1st Version

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGitHub1
+//
+//  Created by Chris Ho on 3/12/14.
+//  Copyright (c) 2014 spca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGitHub1
+//
+//  Created by Chris Ho on 3/12/14.
+//  Copyright (c) 2014 spca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
